4713b60e0ec1c71b7edb6b98c3da88438879c4b5
曾瑞 <zengrui@xiaozhu.com>
2019-12-06 18:24:47 +0800

    add akka dependent

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,19 @@
-object Dependencies {
+import sbt._
 
+
+object Dependencies {
+  // Compile dependencies
+
+  // format: OFF
+
+  private val akka                           = "com.typesafe.akka"                   %% "akka-actor"                      % "2.6.0"
+  private val akkaSlf4j                      = akka.organization                     %% "akka-slf4j"                      % akka.revision
+  private val akkaStream                     = akka.organization                     %% "akka-stream"                     % akka.revision
+  private val akkaProtobuf                   = akka.organization                     %% "akka-protobuf"                   % akka.revision
+  private val akkaTestKit                    = akka.organization                     %% "akka-testkit"                    % akka.revision       % "test"
+  
+  
+  val akkaDependencies = Seq(
+    akka,akkaSlf4j,akkaProtobuf,akkaTestKit
+  )
 }
